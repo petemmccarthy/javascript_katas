1f996226f695387efa3b126ab8728b726d090825
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-02-10 19:48:07 +0000

    file structure simplified

--- jasmine-standalone-2.1.3/src/Questions.js
+++ jasmine-standalone-2.1.3/src/Questions.js
@@ -94,3 +94,157 @@ function returnFirstHalfOfString(string) {
 function makeNumberNegative(number) {
   return number < 0 ? number : number * -1;
 };
+
+// Question 11
+
+function separateArrayIntoEvenAndOddNumbers(array) {
+
+};
+
+// Question 12
+
+function numberOfElementsThatArePalindromes(array) {
+
+};
+
+// Question 13
+
+function shortestWordInArray(array) {
+
+};
+
+// Question 14
+
+function longestWordInArray(array) {
+
+};
+
+// Question 15
+
+function arrayTotal(array) {
+
+};
+
+// Question 16
+
+function doubleArray(array) {
+
+};
+
+// Question 17
+
+function averageOfArray(array) {
+
+};
+
+// Question 18
+
+function removeElementsGreaterThanFive(array) {
+
+};
+
+function isLessThanFive(element) {
+
+};
+
+// Question 19
+
+function convertArrayToObject(array) {
+
+};
+
+// Question 20
+
+function getLettersInArrayOfWords(array) {
+
+};
+
+// Question 21
+
+function swapKeysAndValuesInObject(object) {
+
+};
+
+// Question 22
+
+function addKeysAndValues(object) {
+
+};
+
+// Question 23
+
+function removeCapitalLettersFromString(string) {
+
+};
+
+// Question 24
+
+function everyPossiblePairing(array) {
+
+};
+
+// Question 25
+
+function roundUpNumber(number) {
+
+};
+
+// Question 26
+
+function roundDownNumber(number) {
+
+};
+
+// Question 27
+
+function formatDateNicely(date) {
+
+};
+
+// Question 28
+
+function getDomainNameFromEmailAddress(email) {
+
+};
+
+// Question 29
+
+function titleizeString(string) {
+
+};
+
+// Question 30
+
+function checkStringForSpecialCharacters(string) {
+
+};
+
+// Question 31
+
+function getUpperLimitOfRange(range) {
+
+};
+
+// Question 32
+
+function squareRootOf(number) {
+
+};
+
+// Question 33
+
+function callMethodFromString(string) {
+
+};
+
+// Question 34
+
+function isA2015BankHoliday(date) {
+
+};
+
+// Question 35
+
+function convertToRomanNumerals(number) {
+
+};
